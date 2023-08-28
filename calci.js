2a2bd08a933a31487defd427be782ea80751dbd4
panther865 <77604848+panther865@users.noreply.github.com>
2023-08-28 10:50:37 +0530

    buttons js added

--- calci.js
+++ calci.js
@@ -1,22 +1,4 @@
-const btnac = document.getElementById("ac");
-const para1 = document.getElementById("(");
-const para2 = document.getElementById(")");
-const btndel = document.getElementById("del");
-const btn1 = document.getElementById("1");
-const btn2 = document.getElementById("2");
-const btn3 = document.getElementById("3");
-const btn4 = document.getElementById("4");
-const btn5 = document.getElementById("5");
-const btn6 = document.getElementById("6");
-const btn7 = document.getElementById("7");
-const btn8 = document.getElementById("8");
-const btn9 = document.getElementById("9");
-const btn0 = document.getElementById("0");
-const add = document.getElementById("+");
-const sub = document.getElementById("-");
-const div = document.getElementById("/");
-const mul = document.getElementById("*");
-const equal = document.getElementById("=");
+
 let input = document.getElementById("input");
 let lable = document.getElementById("lable");
 function butn1(){
@@ -47,10 +29,7 @@ function butn7(){
     var input = document.getElementById("input");
     input.value+="7";
 }
-function butn7(){
-    var input = document.getElementById("input");
-    input.value+="7";
-}
+
 function butn8(){
     var input = document.getElementById("input");
     input.value+="8";
